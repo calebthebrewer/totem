b45cc10b731a0768c3d496e6d7ea73ef221f7b5d
calebthebrewer <calebthebrewer@gmail.com>
2015-03-18 19:46:42 -0500

    backing up

--- client/development/find/find.controller.js
+++ client/development/find/find.controller.js
@@ -1,15 +1,22 @@
 angular.module('totem.find')
-.controller('find', [
+	.controller('find', [
 		'$scope',
 		'totems',
-		function($scope, totems) {
+		function ($scope, totems) {
+			var search = null;
 			$scope.searchTotems = function searchTotems() {
 				var query = $scope.totemName;
 
 				totems
 					.search(query)
-					.then(function(totems) {
+					.then(function (totems) {
 						$scope.foundTotems = totems;
+
+						if (!totems.length) {
+							$scope.searchMessage = 'Nothing found by that name.';
+						} else {
+							$scope.searchMessage = '';
+						}
 					});
 			};
 		}
